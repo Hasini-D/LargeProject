6f0b98899b463bec71698374f7619d5ec3fe039e
Kenneth Valladares <ke615249@ucf.edu>
2025-03-12 21:44:54 -0400

    Changed the routing

--- frontend/src/components/Path.ts
+++ frontend/src/components/Path.ts
@@ -1,6 +1,6 @@
 export function buildPath(route: string): string {
     if (process.env.NODE_ENV === 'production') {
-      return 'http://fitjourneyhome.com:5001/' + route;  // Use your domain here
+      return 'http://fitjourneyhome.com:5001/${route}';  // Use your domain here
     } else {
       return 'http://localhost:5001/' + route;  // Keep localhost for local development
     }
