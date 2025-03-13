3ce048a7a726b2cec0c5c39a37335269a8512c89
Kenneth Valladares <ke615249@ucf.edu>
2025-03-12 23:13:20 -0400

    Removed the /api again

--- frontend/src/components/Path.ts
+++ frontend/src/components/Path.ts
@@ -1,5 +1,5 @@
 export function buildPath(route: string): string {
     // Since Nginx proxies /api to the backend, you don't include the port here.
-    return `http://fitjourneyhome.com/api/${route}`;
+    return `http://fitjourneyhome.com/${route}`;
   }
   
