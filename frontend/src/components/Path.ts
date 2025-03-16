9975f6481e90429383d9a430b47a3ebe4ca342ab
Kenneth Valladares <ke615249@ucf.edu>
2025-03-16 13:16:02 -0400

    Added an if else to support local hosting.

--- frontend/src/components/Path.ts
+++ frontend/src/components/Path.ts
@@ -1,5 +1,8 @@
 export function buildPath(route: string): string {
     // Since Nginx proxies /api to the backend, you don't include the port here.
-    return `http://fitjourneyhome.com/${route}`;
+    if(process.env.NODE_ENV === 'production'){
+      return `http://fitjourneyhome.com/${route}`;
+    }else{
+      return `http://localhost:5001/${route}`;
+    }
   }
-  
