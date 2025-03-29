6dde4ebf9646117f5e74d5d4806f123a77d701e6
alexusar <alexander.garcia31837@gmail.com>
2025-03-29 14:56:23 -0400

    Fix Frontend Deployment

--- frontend/frontend-deploy.sh
+++ frontend/frontend-deploy.sh
@@ -6,13 +6,13 @@ SERVER_USER="root"
 SERVER_IP="fitjourneyhome.com"
 REMOTE_DIR="/var/www/html"
 
-cd "$PROJECT_DIR" || { echo "Project directory not found!"; exit 1; }
+cd "$PROJECT_DIR"  { echo "Project directory not found!"; exit 1; }
 
 echo "Building the project..."
-npm run build || { echo "Build failed!"; exit 1; }
+npm run build  { echo "Build failed!"; exit 1; }
 
 echo "Copying files to server..."
-sshpass -p "$DEPLOY_PASSWORD" scp -o PreferredAuthentications=password -o PubkeyAuthentication=no -v -r "$BUILD_DIR"/* "$SERVER_USER@$SERVER_IP:$REMOTE_DIR" || { echo "File transfer failed!"; exit 1; }
+sshpass -f ../pw.txt scp -r "$BUILD_DIR"/* "$SERVER_USER@$SERVER_IP:$REMOTE_DIR" || { echo "File transfer failed!"; exit 1; }
 
 
 echo "Deployment successful!"
