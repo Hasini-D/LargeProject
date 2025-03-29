0b7c8c88d0452c59c12bfb93986e9887b8048d50
alexusar <alexander.garcia31837@gmail.com>
2025-03-29 12:35:15 -0400

    Update UI

--- frontend/src/components/CalendarUI.tsx
+++ frontend/src/components/CalendarUI.tsx
@@ -1,21 +1,21 @@
-// import React, { useState } from "react";
-// import Calendar from "react-calendar";
+import React, { useState } from "react";
+import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css"; 
 import IconUI from "./IconsUI";
 
+type ValuePiece = Date | null;
+type Value = ValuePiece | [ValuePiece, ValuePiece];
 function CalendarUI() {
-
-    //   const [date, setDate] = useState(new Date());
-
+    const [date, setDate] = useState<Value>(new Date());
     return (
         <div className="h-screen bg-white flex flex-col">
             {/* Top Bar */}
             <IconUI/>
-
             <h2 className="text-3xl font-bold text-center text-#0f172a mb-6">Calendar</h2>
-            {/* {<Calendar onChange={setDate} value={date} /> } */}
+            <div className="flex justify-center">
+                <Calendar onChange={setDate} value={date} />
+            </div>
         </div>
     );
 }
-
 export default CalendarUI;
